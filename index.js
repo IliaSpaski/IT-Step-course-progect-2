3a898a042379bbad2e1042ae6f89f0aebfd5aeb2
Ilia <92109442+IliaSpaski@users.noreply.github.com>
2023-03-09 18:33:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove('active-content');
+        tabcontent[i].classList.add('hidden-content');
+
+
       tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    const activeContent = document.getElementById(cityName);
+    activeContent.style.display = "block";
+
+      activeContent.classList.remove('hidden-content');
+
+      setTimeout(() => {
+        activeContent.classList.add('active-content');
+      },300)
+     
+
     evt.currentTarget.className += " active";
   }
